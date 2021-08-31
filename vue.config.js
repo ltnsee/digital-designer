384f3e50219915f272eb1e468dbc637ee277f7bf
ltnsee <31405056+ltnsee@users.noreply.github.com>
2021-08-31 11:16:39 +0800

    配置vueconfig

---
+++ vue.config.js
@@ -0,0 +1,21 @@
+'use strict';
+const path = require('path');
+
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+
+module.exports = {
+  publicPath: '/',
+  outputDir: 'dist',
+  assetsDir: '/static',
+  lintOnSave: process.env.NODE_ENV === 'development',
+  productionSourceMap: false,
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': resolve('src'),
+      },
+    },
+  },
+};
